56fb420dcd37d7ba32dffdcb0d92ea7e849bbd3f
MiklosHorvath <horvathmikloswork@gmail.com>
2019-12-18 10:12:38 +0100

    Add suppliers to DAO.

--- src/main/java/com/codecool/shop/config/Initializer.java
+++ src/main/java/com/codecool/shop/config/Initializer.java
@@ -24,10 +24,12 @@ public class Initializer implements ServletContextListener {
         SupplierDao supplierDataStore = SupplierDaoMem.getInstance();
 
         //setting up a new supplier
-        Supplier amazon = new Supplier("Amazon", "Digital content and services");
-        supplierDataStore.add(amazon);
-        Supplier lenovo = new Supplier("Lenovo", "Computers");
-        supplierDataStore.add(lenovo);
+        Supplier superFlowers = new Supplier("Super Flowers kft", "Small family business with quality plants");
+        supplierDataStore.add(superFlowers);
+        Supplier kingOfCactus = new Supplier("King Of Cactus kft", "Main priority is cacti, but also sell other plants");
+        supplierDataStore.add(kingOfCactus);
+        Supplier bigFlowerBusiness = new Supplier("Big Flower Business Zrt", "Industry leader big company");
+        supplierDataStore.add(bigFlowerBusiness);
 
         //setting up a new product category
         ProductCategory cactus = new ProductCategory("Cactus", "Plant", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.");
@@ -40,9 +42,9 @@ public class Initializer implements ServletContextListener {
         productCategoryDataStore.add(palm);
 
         //setting up products and printing it
-        productDataStore.add(new Product("Amazon Fire", 49.9f, "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", cactus, amazon));
-        productDataStore.add(new Product("Lenovo IdeaPad Miix 700", 479, "USD", "Keyboard cover is included. Fanless Core m5 processor. Full-size USB ports. Adjustable kickstand.", cactus, lenovo));
-        productDataStore.add(new Product("Amazon Fire HD 8", 89, "USD", "Amazon's latest Fire HD 8 tablet is a great value for media consumption.", cactus, amazon));
-        productDataStore.add(new Product("Test", 89, "USD", "Test product", cactus, amazon));
+        productDataStore.add(new Product("Amazon Fire", 49.9f, "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", cactus, superFlowers));
+        productDataStore.add(new Product("Lenovo IdeaPad Miix 700", 479, "USD", "Keyboard cover is included. Fanless Core m5 processor. Full-size USB ports. Adjustable kickstand.", cactus, kingOfCactus));
+        productDataStore.add(new Product("Amazon Fire HD 8", 89, "USD", "Amazon's latest Fire HD 8 tablet is a great value for media consumption.", cactus, bigFlowerBusiness));
+        productDataStore.add(new Product("Test", 89, "USD", "Test product", cactus, bigFlowerBusiness));
     }
 }
