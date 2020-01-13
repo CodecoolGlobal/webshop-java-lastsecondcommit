ec0ada953cb5d134cc3d2cae4c1695fdb47c1baa
Eszter Kobor <kobor.eszter@gmail.com>
2020-01-13 12:03:22 +0100

    Add suppliers to DB

--- src/main/sql/create_database.sql
+++ src/main/sql/create_database.sql
@@ -1,4 +1,23 @@
 DROP TABLE IF EXISTS product;
+DROP TABLE IF EXISTS product_category;
+DROP TABLE IF EXISTS supplier;
+
+CREATE TABLE product_category
+(
+id serial PRIMARY KEY,
+name varchar(40),
+description text,
+department varchar(40)
+
+);
+
+CREATE TABLE supplier
+(
+    id serial PRIMARY KEY,
+    name varchar(40),
+    description text
+);
+
 CREATE TABLE product
 (
     id serial PRIMARY KEY,
@@ -12,26 +31,12 @@ CREATE TABLE product
 
 );
 
-DROP TABLE IF EXISTS product_category;
-CREATE TABLE product_category
-(
-    id serial PRIMARY KEY,
-    name varchar(40),
-    description text,
-    department varchar(40)
+INSERT INTO supplier (name, description)
+VALUES ('Super Flowers Kft.', 'Small family business with quality plants'),
+('King Of Cactus Kft.', 'Main priority is cacti, but also sell other plants'),
+('Big Flower Business Zrt.', 'Industry leader big company'),
+('Green Future Kft.', 'Selling wide range of plants grown in the country');
 
-);
-
-DROP TABLE IF EXISTS supplier;
-CREATE TABLE supplier
-(
-    id serial PRIMARY KEY,
-    name varchar(40),
-    description text
-);
-
-INSERT INTO product (name, description, default_price, default_currency, product_category, supplier_id, product_category_id)
-VALUES ("")
 
 
 
