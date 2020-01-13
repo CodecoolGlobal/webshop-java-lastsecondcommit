2320c4a2b8883cecc8e3e2649fe3f9becc40a809
MiklosHorvath <horvathmikloswork@gmail.com>
2020-01-13 22:37:33 +0100

    implements productDaoJDBC add.

--- src/main/java/com/codecool/shop/dao/implementation/ProductDaoJDBC.java
+++ src/main/java/com/codecool/shop/dao/implementation/ProductDaoJDBC.java
@@ -5,16 +5,37 @@ import com.codecool.shop.model.Product;
 import com.codecool.shop.model.ProductCategory;
 import com.codecool.shop.model.Supplier;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
 public class ProductDaoJDBC extends JDBC implements ProductDao {
     @Override
     public void add(Product product) {
+        String query = "INSERT INTO product (name, description, default_price, default_currency, supplier_id, product_category_id)" +
+                " VALUES (?,?,?,?,?,?)";
 
+        try (PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setString(1, product.getName());
+            statement.setString(2, product.getDescription());
+            statement.setFloat(3, product.getDefaultPrice());
+            statement.setString(4, product.getDefaultCurrency().toString());
+            statement.setInt(5, product.getSupplier().getId());
+            statement.setInt(6, product.getProductCategory().getId());
+            statement.executeQuery();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public Product find(int id) {
+        String query = "SELECT * WHERE id = ? ";
+        try (PreparedStatement statement = connection.prepareStatement(query)){
+            statement.setInt(1, id);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
