212a26504c2dcc08d57f3f5316ee669eec9fb0f5
Eszter Kobor <kobor.eszter@gmail.com>
2020-01-14 11:15:42 +0100

    Implement supplierDaoJDBC

--- src/main/java/com/codecool/shop/dao/implementation/SupplierDaoJDBC.java
+++ src/main/java/com/codecool/shop/dao/implementation/SupplierDaoJDBC.java
@@ -3,26 +3,81 @@ package com.codecool.shop.dao.implementation;
 import com.codecool.shop.dao.SupplierDao;
 import com.codecool.shop.model.Supplier;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 public class SupplierDaoJDBC extends JDBC implements SupplierDao {
     @Override
     public void add(Supplier supplier) {
-
+        String query = "INSERT INTO supplier (name, description) VALUES (?, ?)";
+        try (PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setString(1, supplier.getName());
+            statement.setString(2, supplier.getDescription());
+            statement.executeQuery();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public Supplier find(int id) {
-        return null;
+        String query = "SELECT * FROM supplier WHERE id = ? ;";
+        ResultSet resultSet = null;
+        Supplier result = null;
+        try (PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setInt(1, id);
+            resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                result = new Supplier(
+                        resultSet.getString("name"),
+                        resultSet.getString("description"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (resultSet != null)
+                    resultSet.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return result;
     }
 
     @Override
     public void remove(int id) {
-
+        String query = "DELETE FROM supplier WHERE id = ?;";
+        try (PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setInt(1, id);
+            statement.executeQuery();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public List<Supplier> getAll() {
-        return null;
+
+        String query = "SELECT * FROM supplier;";
+        List<Supplier> resultList = new ArrayList<>();
+
+        try (Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(query)) {
+            while (resultSet.next()) {
+                Supplier supplier = new Supplier(
+                        resultSet.getString("name"),
+                        resultSet.getString("description"));
+                resultList.add(supplier);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return resultList;
     }
+
 }
