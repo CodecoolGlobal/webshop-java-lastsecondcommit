e59893a06ac5ef4bd252e1b6558674a491f2a730
Eszter Kobor <kobor.eszter@gmail.com>
2020-01-14 10:53:04 +0100

    Add getByProductCategory

--- src/main/java/com/codecool/shop/dao/implementation/ProductDaoJDBC.java
+++ src/main/java/com/codecool/shop/dao/implementation/ProductDaoJDBC.java
@@ -6,10 +6,15 @@ import com.codecool.shop.model.ProductCategory;
 import com.codecool.shop.model.Supplier;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ProductDaoJDBC extends JDBC implements ProductDao {
+
+    SupplierDaoJDBC supplierDaoJDBC = new SupplierDaoJDBC();
+
     @Override
     public void add(Product product) {
         String query = "INSERT INTO product (name, description, default_price, default_currency, supplier_id, product_category_id)" +
@@ -31,7 +36,7 @@ public class ProductDaoJDBC extends JDBC implements ProductDao {
     @Override
     public Product find(int id) {
         String query = "SELECT * FROM product WHERE id = ? ";
-        try (PreparedStatement statement = connection.prepareStatement(query)){
+        try (PreparedStatement statement = connection.prepareStatement(query)) {
             statement.setInt(1, id);
         } catch (SQLException e) {
             e.printStackTrace();
@@ -56,6 +61,35 @@ public class ProductDaoJDBC extends JDBC implements ProductDao {
 
     @Override
     public List<Product> getBy(ProductCategory productCategory) {
-        return null;
+        ResultSet resultSet = null;
+        int productCategoryId = productCategory.getId();
+        String query = "SELECT * FROM product WHERE id = ?;";
+        List<Product> resultList = new ArrayList<>();
+
+        try (PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setInt(1, productCategoryId);
+            resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                Product product = new Product(
+                        resultSet.getString("name"),
+                        resultSet.getFloat("default_price"),
+                        resultSet.getString("default_currency"),
+                        resultSet.getString("description"),
+                        productCategory,
+                        supplierDaoJDBC.find(resultSet.getInt("supplier_id")));
+                resultList.add(product);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (resultSet != null)
+                    resultSet.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return resultList;
     }
 }
+
