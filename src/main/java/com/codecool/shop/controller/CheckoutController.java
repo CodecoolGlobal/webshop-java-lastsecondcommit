9a546e5c65c6835ca02e349ceedee7bf1e9e0efb
Eszter Kobor <kobor.eszter@gmail.com>
2020-01-15 16:29:31 +0100

    Add checkoutpage

---
+++ src/main/java/com/codecool/shop/controller/CheckoutController.java
@@ -0,0 +1,27 @@
+package com.codecool.shop.controller;
+
+import com.codecool.shop.config.TemplateEngineUtil;
+import com.codecool.shop.dao.ShoppingCartDao;
+import com.codecool.shop.dao.implementation.Mem.ShoppingCartDaoMem;
+import com.codecool.shop.model.ShoppingCart;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = {"/checkout"})
+public class CheckoutController extends HttpServlet {
+
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
+        WebContext context = new WebContext(req, resp, req.getServletContext());
+        engine.process("product/checkout.html", context, resp.getWriter());
+
+    }
+
+}
