cb16cbe09ecd74ec2758246c62b3bed0ac95dccd
Paebo <petibors2@gmail.com>
2020-01-16 12:51:30 +0100

    Clicking "+ & -" buttons change quantity in cart done and also changes "Items in cart" in the header

--- src/main/java/com/codecool/shop/controller/CartController.java
+++ src/main/java/com/codecool/shop/controller/CartController.java
@@ -1,9 +1,15 @@
 package com.codecool.shop.controller;
 
 import com.codecool.shop.config.TemplateEngineUtil;
+import com.codecool.shop.dao.ProductDao;
 import com.codecool.shop.dao.ShoppingCartDao;
+import com.codecool.shop.dao.implementation.Mem.ProductDaoMem;
 import com.codecool.shop.dao.implementation.Mem.ShoppingCartDaoMem;
+import com.codecool.shop.model.Product;
 import com.codecool.shop.model.ShoppingCart;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 
@@ -12,6 +18,8 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.BufferedReader;
 import java.io.IOException;
 
 @WebServlet(urlPatterns = {"/cart"})
@@ -24,9 +32,6 @@ public class CartController extends HttpServlet {
     private static final String ID_NAME = "product_id";
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-    }
-
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
         WebContext context = new WebContext(req, resp, req.getServletContext());
@@ -36,4 +41,46 @@ public class CartController extends HttpServlet {
 
     }
 
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        System.out.println("work");
+
+        ProductDao productDataStore = ProductDaoMem.getInstance();
+        ShoppingCartDao cartDataStore = ShoppingCartDaoMem.getInstance();
+
+        ShoppingCart shoppingCart = cartDataStore.find(1);
+
+        WebContext context = new WebContext(req, resp, req.getServletContext());
+        HttpSession session = req.getSession();
+
+        StringBuilder buffer = new StringBuilder();
+        BufferedReader reader = req.getReader();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            buffer.append(line);
+        }
+
+        String data = buffer.toString();
+        JsonParser jsonParser = new JsonParser();
+        JsonElement jsonElement = jsonParser.parse(data);
+        String id = "invalid";
+        if (jsonElement.isJsonObject()) {
+            JsonObject partialProduct = jsonElement.getAsJsonObject();
+            if (partialProduct.has(ID_NAME)) {
+                id = partialProduct.get(ID_NAME).getAsString();
+            } else {
+                // TODO client error
+            }
+        } else {
+            // TODO client error
+        }
+        // TODO: try/catch  (client error)
+        Product product = productDataStore.find(Integer.parseInt(id));
+        shoppingCart.add(product);
+    }
 }
+
+
+
+
--- src/main/webapp/static/js/dom.js
+++ src/main/webapp/static/js/dom.js
@@ -3,28 +3,30 @@ import { dataHandler } from "./data_handler.js";
 export let dom = {
 
     init: function () {
-        dom.addEventListenerToFilterCheckbox();
+        // dom.addEventListenerToFilterCheckbox();
         dom.addEventListenerToCartButtons();
+        dom.addEventListenerToMinusButtons();
+        dom.addEventListenerToPlusButtons();
     },
 
     addEventListenerToCartButtons: function () {
-        let addToCartButtons = document.querySelectorAll(".btn-success");
+        let addToCartButtons = document.querySelectorAll(".add-cart");
 
         addToCartButtons.forEach((button) => {
             let productId = button.dataset.productId;
             button.addEventListener('click', function () {
-                dataHandler.addToCart(productId, dom.increaseNumOfItemsInCart())
+                dataHandler.addToCart(productId, dom.changeNumOfItemsInCart(1))
             });
         });
     },
 
-    increaseNumOfItemsInCart: function () {
+    changeNumOfItemsInCart: function (changeValue) {
         let itemsInCart = parseInt(document.querySelector("#cart-info").innerHTML);
-        itemsInCart += 1;
+        itemsInCart += changeValue;
         document.querySelector("#cart-info").innerHTML = itemsInCart.toString();
     },
 
-    addEventListenerToFilterCheckbox :function () {
+    /*addEventListenerToFilterCheckbox :function () {
         let filterForm = document.querySelector('#filters');
         let checkboxes = filterForm.querySelectorAll("input");
 
@@ -33,5 +35,36 @@ export let dom = {
                 filterForm.submit();
             })
         }
+    },*/
+
+    addEventListenerToMinusButtons: function () {
+        let quantityChangeButtons = document.querySelectorAll(".btn-minus");
+
+        quantityChangeButtons.forEach((button) => {
+            button.addEventListener('click', function () {
+                dom.changeQuantity(button, -1)
+
+            });
+        });
+    },
+
+    addEventListenerToPlusButtons: function () {
+        let quantityChangeButtons = document.querySelectorAll(".btn-plus");
+
+        quantityChangeButtons.forEach((button) => {
+            button.addEventListener('click', function () {
+                dom.changeQuantity(button, 1)
+
+            });
+        });
+    },
+
+    changeQuantity: function (button, changeValue) {
+        let rowNode = button.closest(".product-row");
+        let quantityNode = rowNode.querySelector("#quantity");
+        let quantity = parseInt(quantityNode.innerHTML);
+        quantity += changeValue;
+        quantityNode.innerHTML = quantity.toString();
+        dom.changeNumOfItemsInCart(changeValue)
     }
 };
