b2b2db5404b5e813c3cc05c01eca1810768dca34
Eszter Kobor <kobor.eszter@gmail.com>
2020-01-27 06:49:34 +0100

    Change shopping cart for linitems

--- src/main/java/com/codecool/shop/model/ShoppingCart.java
+++ src/main/java/com/codecool/shop/model/ShoppingCart.java
@@ -4,42 +4,10 @@ import java.util.*;
 
 public class ShoppingCart {
     private int orderId;
-    private List<Product> products = new ArrayList<>();
+    private List<LineItem> lineItems = new ArrayList<>();
 
-    public List<Product> getProducts() {
-        return products;
-    }
-
-    public void add(Product product) {
-        products.add(product);
-    }
-
-    public void remove(Product product) {products.remove(product); }
-
-    public int getItemNumberInCart() {
-        return products.size();
-    }
-
-    public Map<Product, Integer> getProductsStat() {
-        Map<Product, Integer> productsStat = new LinkedHashMap<>();
-        for (Product product : products) {
-            Integer count = productsStat.get(product);
-            if (count == null) {
-                productsStat.put(product, 1);
-            }
-            else {
-                productsStat.put(product, count + 1);
-            }
-        }
-        return productsStat;
-    }
-
-    public float getSumOfPrices() {
-        float sumOfPrices = 0;
-        for (Product product : products) {
-           sumOfPrices += product.getDefaultPrice();
-        }
-        return sumOfPrices;
+    public List<LineItem> getLineItems() {
+        return lineItems;
     }
 
     public int getOrderId() {
@@ -48,5 +16,27 @@ public class ShoppingCart {
 
     public void setOrderId(int orderId) {
         this.orderId = orderId;
+    }
+
+    public void add(LineItem lineItem) {
+        lineItems.add(lineItem);
+    }
+
+    public void remove(LineItem lineItem) {lineItems.remove(lineItem); }
+
+    public int getItemNumberInCart() {
+        int itemNumber = 0;
+        for (LineItem lineItem : lineItems) {
+            itemNumber += lineItem.getQuantity();
+        }
+        return itemNumber;
+    }
+
+    public float getSumOfPrices() {
+        int sum = 0;
+        for (LineItem lineItem : lineItems) {
+            sum += lineItem.getPrice();
+        }
+        return sum;
     }
 }
