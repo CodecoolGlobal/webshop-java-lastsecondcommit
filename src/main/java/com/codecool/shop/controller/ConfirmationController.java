3f285365b84f59148328e1b8cb58c60282d3ce66
MiklosHorvath <horvathmikloswork@gmail.com>
2020-01-28 13:45:46 +0100

    implements a non functioning confirmation page appear after clicking pay.

---
+++ src/main/java/com/codecool/shop/controller/ConfirmationController.java
@@ -0,0 +1,23 @@
+package com.codecool.shop.controller;
+
+import com.codecool.shop.config.TemplateEngineUtil;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = {"/confirmation"})
+public class ConfirmationController extends CartController {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
+        WebContext context = new WebContext(req, resp, req.getServletContext());
+        setupShoppingCart(req);
+        context.setVariable("shoppingCart", shoppingCart);
+        engine.process("product/confirmation.html", context, resp.getWriter());
+    }
+}
