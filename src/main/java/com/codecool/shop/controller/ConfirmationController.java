6220dceb82a2c3bd277fd6c724b976de7d79e92d
MiklosHorvath <horvathmikloswork@gmail.com>
2020-01-29 12:53:17 +0100

    implements email contain order id and create test json file

--- src/main/java/com/codecool/shop/controller/ConfirmationController.java
+++ src/main/java/com/codecool/shop/controller/ConfirmationController.java
@@ -2,6 +2,7 @@ package com.codecool.shop.controller;
 
 import com.codecool.shop.config.TemplateEngineUtil;
 import org.apache.log4j.xml.DOMConfigurator;
+import org.json.simple.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.thymeleaf.TemplateEngine;
@@ -11,6 +12,7 @@ import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.FileWriter;
 import java.io.IOException;
 
 @WebServlet(urlPatterns = {"/confirmation"})
@@ -24,12 +26,14 @@ public class ConfirmationController extends CartController {
         setupShoppingCart(req);
         context.setVariable("shoppingCart", shoppingCart);
         engine.process("product/confirmation.html", context, resp.getWriter());
-        mailUtility.sendMail("mindentnagyonjolprogramozomiki@gmail.com", "succesfull order from ourbestplants", "you succesfully ordered our best plants. be carefull with the cactus");
+        mailUtility.sendMail("mindentnagyonjolprogramozomiki@gmail.com", "succesfull order from ourbestplants", "you succesfully ordered our best plants. be carefull with the cactus \n"+
+                "your order id is:" + String.valueOf(shoppingCart.getOrderId()));
 
-
-
-
-
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("testkey", "testvalue");
+        FileWriter file = new FileWriter("/media/rootMX18.3/home/miki/codecool/oop/6th_TW/webshop-java-lastsecondcommit/orders/order.json");
+        file.write(jsonObject.toJSONString());
+        file.close();
         logger.info("Finnished processing POST request for url: '/confirmation'. session id: {} order id: {}", req.getSession().getId(), String.valueOf(shoppingCart.getOrderId()));
     }
 }
